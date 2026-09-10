8bc16ded92d24a7bcd5a78b73ee7daae0b7effe1
isaac-gainline <144013555+isaac-gainline@users.noreply.github.com>
2026-09-10 09:05:47 +0100

    SES-3101: Enable club portal login v2 and checkout copy

---
+++ src/ClubPortal/LoginCopy.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\ClubPortal;
+
+final class LoginCopy
+{
+    public const HEADLINE = 'Sign in to your club account';
+
+    public const BANNER = 'Members save time with saved details';
+}
